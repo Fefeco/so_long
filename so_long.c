0a27e0f8f005a481910d9e152dfc45c49ec79139
Federico Carranza <fcarranz@student.42barcelona.com>
2024-03-28 21:25:35 +0100

    Ya cierra bien el programa. Estoy viendo como mover al personaje

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 09:52:26 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/03/28 14:32:11 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/03/28 21:23:11 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,22 @@
 ///////////////////////////////
 // Funciones para los Hooks //
 ///////////////////////////////
-int	key_press(int key, t_game *game)
+int	exit_program(win_t *game)
+{
+	if (game)
+		mlx_destroy_window(game->connection, game->window);
+	printf("Cerro!");
+	exit(EXIT_FAILURE);
+}
+
+int	key_press(int key, win_t *game)
 {
 	if (key == 53)
-	{
-//		mlx_clear_window(game->mlx_connection, game->window);
-		mlx_destroy_window(game->mlx_connection, game->window);
-//		mlx_put_image_to_window(ply->mlx_ptr, ply->win_ptr, ply->img, 45, 30);
-	}
+		exit_program(game);
 	else if (key == 124)
 	{
-		mlx_destroy_image(game->mlx_connection, game->player);
-		mlx_put_image_to_window(game->mlx_connection,
-			   	game->window,
-			   	game->player,
-				45, 30);
+		game->box_x += 20;
+		printf("Brujo en posicion \nx = %d\ny = %d\n", game->box_x, game->box_y);
 	}
 	else
 		printf("Tecla %d presionada\n", key);
@@ -57,22 +58,41 @@ int	mouse_click(int button)
 int main(void)
 {
 	win_t	game;
+	void	*player;
+	void	*texture;
 
-	game = ft_create_window(600, 400, "My Juego");
+	game = ft_create_window(WIDTH, HEIGHT, "My Juego");
 	if (!game.connection || !game.window)
 		return (1);
 	
 	
-	game.texture = mlx_xpm_file_to_image(game.connection, "img/textura.xpm", &game.width, &game.height);
-	game.player = mlx_xpm_file_to_image(game.connection, "img/player.xpm", &game.width, &game.height);
+	texture = mlx_xpm_file_to_image(game.connection, 
+			"img/textura.xpm", 
+			&game.width, 
+			&game.height);
+	player = mlx_xpm_file_to_image(game.connection, 
+			"img/player.xpm", 
+			&game.width, 
+			&game.height);
+	
+	mlx_put_image_to_window(game.connection, game.window, texture, 0, 0);
 
-	mlx_put_image_to_window(game.connection, game.window, game.texture, 0, 0);
-	mlx_put_image_to_window(game.connection, game.window, game.player, 25, 30);
-	mlx_string_put(game.connection, game.window, WIDTH * 0.4, HEIGHT * 0.9 , 0xFF99FF, "GAME ON!");
+	game.box_x = 25;
+	game.box_y = 30;
+	mlx_put_image_to_window(game.connection, game.window, player, 
+			game.box_x, game.box_y);
+	mlx_string_put(game.connection, 
+			game.window, 
+			WIDTH * 0.4, 
+			HEIGHT * 0.9 , 
+			0xFF99FF, 
+			"GAME ON!");
 
 	mlx_hook(game.window, 2, 1L << 0, key_press, &game);
-	mlx_hook(game.window, 6, 1L << 6, mouse_move, NULL);
-	mlx_hook(game.window, 4, 1L << 2, mouse_click, NULL);
+//	mlx_hook(game.window, 6, 1L << 6, mouse_move, NULL);
+//	mlx_hook(game.window, 4, 1L << 2, mouse_click, NULL);
+	mlx_hook(game.window, 17, 0, exit_program, &game); // Codigo DestroyNotify
+	mlx_do_sync(game.connection);
 	mlx_loop(game.connection);
 	mlx_destroy_window(game.connection, game.window);
 	free (game.connection);
--- so_long.h
+++ so_long.h
@@ -6,22 +6,35 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 14:02:33 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/03/28 14:19:04 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/03/28 21:12:49 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include <stdlib.h>
 
+#ifndef WIDTH
+# define WIDTH 600
+#endif
+
+#ifndef HEIGHT
+# define HEIGHT 400
+#endif
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
+
+# define ESC 53
+
 typedef	struct win_s {
 	void	*connection;
 	void	*window;
-	int		widh;
+	int		width;
 	int		height;
+	int		box_x;
+	int		box_y;
 } win_t;
-# endif
+#endif
 
 win_t	ft_create_window(int width, int height, char *str);
 
