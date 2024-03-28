1476f31bae320e103dbd1cfcca8d83d1a3a7b6eb
Federico Carranza <fcarranz@student.42barcelona.com>
2024-03-28 14:34:28 +0100

    mas pruebas

--- so_long.c
+++ so_long.c
@@ -6,26 +6,12 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 09:52:26 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/03/28 13:51:21 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/03/28 14:32:11 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include "../mlx/mlx.h"
-
-#define WIDTH 600
-#define HEIGHT 400
-
-typedef struct s_game
-{
-	void	*mlx_connection;
-	void	*window;
-	void	*texture;
-	void	*player;
-	int		height;
-	int		width;
-}	t_game;
+#include "so_long.h"
 
 
 ///////////////////////////////
@@ -70,33 +56,25 @@ int	mouse_click(int button)
 
 int main(void)
 {
-	t_game	game;
+	win_t	game;
 
-    game.mlx_connection = mlx_init();
-	if (!game.mlx_connection)
-	{
-		printf("Fallo en la coneccion al XServer\n");
+	game = ft_create_window(600, 400, "My Juego");
+	if (!game.connection || !game.window)
 		return (1);
-	}
-	game.window = mlx_new_window(game.mlx_connection, WIDTH, HEIGHT, "Mi juego");
-	if (!game.window)
-	{
-		printf("Error al crear la ventana\n");
-		free (game.mlx_connection);
-		return (1);
-	}
-	game.texture = mlx_xpm_file_to_image(game.mlx_connection, "img/textura.xpm", &game.width, &game.height);
-	game.player = mlx_xpm_file_to_image(game.mlx_connection, "img/player.xpm", &game.width, &game.height);
+	
+	
+	game.texture = mlx_xpm_file_to_image(game.connection, "img/textura.xpm", &game.width, &game.height);
+	game.player = mlx_xpm_file_to_image(game.connection, "img/player.xpm", &game.width, &game.height);
 
-	mlx_put_image_to_window(game.mlx_connection, game.window, game.texture, 0, 0);
-	mlx_put_image_to_window(game.mlx_connection, game.window, game.player, 25, 30);
-	mlx_string_put(game.mlx_connection, game.window, WIDTH * 0.4, HEIGHT * 0.9 , 0xFF99FF, "GAME ON!");
+	mlx_put_image_to_window(game.connection, game.window, game.texture, 0, 0);
+	mlx_put_image_to_window(game.connection, game.window, game.player, 25, 30);
+	mlx_string_put(game.connection, game.window, WIDTH * 0.4, HEIGHT * 0.9 , 0xFF99FF, "GAME ON!");
 
 	mlx_hook(game.window, 2, 1L << 0, key_press, &game);
 	mlx_hook(game.window, 6, 1L << 6, mouse_move, NULL);
 	mlx_hook(game.window, 4, 1L << 2, mouse_click, NULL);
-	mlx_loop(game.mlx_connection);
-	mlx_destroy_window(game.mlx_connection, game.window);
-	free (game.mlx_connection);
+	mlx_loop(game.connection);
+	mlx_destroy_window(game.connection, game.window);
+	free (game.connection);
 	return (0);
 }
