0e2f7893a249f560022c7807b09fc9399449515b
Federico Carranza <fcarranz@student.42barcelona.com>
2024-04-02 14:02:47 +0200

    map.c | correcciones para que pase norminette

--- map.c
+++ map.c
@@ -6,13 +6,13 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 11:29:34 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/04/02 13:40:41 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/04/02 13:59:46 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int		ft_check_map_size(char *map_file)
+int	ft_check_map_size(char *map_file)
 {
 	int		x;
 	int		y;
@@ -33,10 +33,7 @@ int		ft_check_map_size(char *map_file)
 		free (line);
 		line = get_next_line(fd);
 		if (line && (int)ft_strlen(line) != x + 1)
-		{
-			free (line);
-			return (0);
-		}
+			return (free (line), 0);
 	}
 	close (fd);
 	if (x <= y)
@@ -44,7 +41,7 @@ int		ft_check_map_size(char *map_file)
 	return (1);
 }
 
-int		ft_load_map(t_win *game, char *map_file)
+int	ft_load_map(t_win *game, char *map_file)
 {
 	if (ft_check_extension(map_file, EXTENSION))
 		exit_program(game);
@@ -54,6 +51,6 @@ int		ft_load_map(t_win *game, char *map_file)
 		exit_program(game);
 	}
 	exit_program(game);
-	return(1);
+	return (1);
 //	game->map = map;
 }
