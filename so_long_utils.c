c068634cf99815da2bb40f9e8194111f456759c4
Federico Carranza <fcarranz@student.42barcelona.com>
2024-04-08 10:51:34 +0200

    Cambio estructura de archivos para cumplir con el subject

--- so_long_utils.c
+++ so_long_utils.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 14:01:32 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/04/08 09:37:33 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/04/08 10:49:24 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,23 +35,23 @@ t_win	ft_create_window(int width, int height, char *str)
 int	ft_load_textures(t_win *game)
 {
 	game->bg.floor = mlx_xpm_file_to_image(game->connection,
-			"img/floor.xpm", &(game->width), &(game->height));
+			"textures/floor.xpm", &(game->width), &(game->height));
 	game->bg.wall = mlx_xpm_file_to_image(game->connection,
-			"img/wall.xpm", &(game->width), &(game->height));
+			"textures/wall.xpm", &(game->width), &(game->height));
 	game->bg.coin = mlx_xpm_file_to_image(game->connection,
-			"img/coin.xpm", &(game->width), &(game->height));
+			"textures/coin.xpm", &(game->width), &(game->height));
 	game->bg.exit = mlx_xpm_file_to_image(game->connection,
-			"img/exit.xpm", &(game->width), &(game->height));
+			"textures/exit.xpm", &(game->width), &(game->height));
 	game->bg.exit_open = mlx_xpm_file_to_image(game->connection,
-			"img/exit_open.xpm", &(game->width), &(game->height));
+			"textures/exit_open.xpm", &(game->width), &(game->height));
 	game->player.right = mlx_xpm_file_to_image(game->connection,
-			"img/player_right.xpm", &(game->width), &(game->height));
+			"textures/player_right.xpm", &(game->width), &(game->height));
 	game->player.up = mlx_xpm_file_to_image(game->connection,
-			"img/player_up.xpm", &(game->width), &(game->height));
+			"textures/player_up.xpm", &(game->width), &(game->height));
 	game->player.left = mlx_xpm_file_to_image(game->connection,
-			"img/player_left.xpm", &(game->width), &(game->height));
+			"textures/player_left.xpm", &(game->width), &(game->height));
 	game->player.down = mlx_xpm_file_to_image(game->connection,
-			"img/player_down.xpm", &(game->width), &(game->height));
+			"textures/player_down.xpm", &(game->width), &(game->height));
 	if (!game->bg.floor || !game->bg.wall || !game->bg.coin
 		|| !game->bg.exit || !game->player.right || !game->player.up
 		|| !game->player.left || !game->player.down)
